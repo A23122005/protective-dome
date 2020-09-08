62dbbca7fa113d8a640cb725121e36843563a540
Tester_No_test <38783518+Ghost-ate-your-brains@users.noreply.github.com>
2020-09-08 21:32:48 +0300

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,14 +1,15 @@
-#from random import randint
 from i2p import socket
-import os
-
-sock = socket.socket()
+#sock = socket.socket()
 i2plo = socket.socket()
 portlol = int(input('Введите порт для локального сервера:'))
-sock.bind(('', portlol))
-sock.listen()
-conn, addr = sock.accept()
-i2plo.connect(("i2p-projekt.i2p", 80))
+#sock.bind(('', portlol))
+print('step 2')
+#sock.listen()
+print('step 3')
+#conn, addr = sock.accept()
+print('step 4')
+i2plo.connect(("q5vc65lf345bakn6z55csafm2oo7hgnuxqzxoyjoj5hhasmjf2ga.b32.i2p", 2389))
+print('step 5')
 while(True):
     while(True):
         login =  str(input('Введите логин:'))
@@ -29,12 +30,13 @@ while(True):
         else:
             break
     #test auth
-    i2plo.send(b"/login "+bytes(login, 'utf-8')+bytes(password, 'utf-8')+bytes(secretkey, 'utf-8'))
-    if (i2plo.recv(1024) == 'ok'):
-        break
-    else:
-        print('Неверный логин, пароль или секретный ключ.')
-
+    #i2plo.send(b"/login "+bytes(login, 'utf-8')+bytes(password, 'utf-8')+bytes(secretkey, 'utf-8'))
+    #if (i2plo.recv(1024) == 'ok'):
+    #    break
+    #else:
+    #    print('Неверный логин, пароль или секретный ключ.')
+    break
 while True:
-    getterl = sock.recv(1024)
-    i2plo.send('/data '+getterl)
+    i2plo.send(b'hello!')
+    getterl = i2plo.recv(1024)
+    print(getterl)
